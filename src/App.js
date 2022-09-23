9aac4bbaab3e8afab1a8f37171652bf9169f2a4c
Brandon E <brandonelliott0594@gmail.com>
2022-09-22 23:39:17 -0600

    added some elements and styling

--- src/App.js
+++ src/App.js
@@ -2,34 +2,36 @@ import "./App.css";
 
 function App() {
   return (
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4 text-light ">Brandon Elliott</h1>
-        <p class="lead text-light">Welcome to my website</p>
-        <ul class="nav">
-          <li class="nav-item">
-            <a class="nav-link active text-light" href="#">
-              About Me
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-light" href="#">
-              Portfolio
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-light" href="#">
-              Resume
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-light" href="#">
-              Contact Me
-            </a>
-          </li>
-        </ul>
+    <>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4 text-light ">Brandon Elliott</h1>
+          <p class="lead text-light">Welcome to my website</p>
+          <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link active text-light" href="#">
+                About Me
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#">
+                Portfolio
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#">
+                Resume
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#">
+                Contact Me
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
